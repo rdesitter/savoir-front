0e830ed21e7289712f6ab7f9b80d795fd1c26efb
Raphael Desitter <rdesitter@gmail.com>
2022-09-22 19:28:13 +0200

    :rocket: feat : display button from logged or not + props custom icon in button

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Button from '../Button';
 import logo from './LOGO.svg';
@@ -6,23 +7,37 @@ import searchIcon from './search.svg';
 import './style.scss';
 
 function AppHeader() {
+  const isLogged = useSelector((state) => state.user.logged);
+
   return (
     <nav className="main-nav" aria-label="Navigation">
       <Link to="/" className="main-nav__logo"><img src={logo} alt="null" /></Link>
       <ul className="main-nav__list">
         <li className="main-nav__item main-nav__item--search">
-          <Link to="recherche" className="main-nav__link"><img src={searchIcon} alt="null" className="main-nav__link__icon" /> Faire une recherche</Link>
+          <Link to="recherche" className="main-nav__link" title="Faire une recherche"><img src={searchIcon} alt="null" className="main-nav__link__icon" /> Faire une recherche</Link>
         </li>
-        <li className="main-nav__item">
-          <Link to="/connection" className="main-nav__link">
-            <Button label="Se connecter" type="outlined" />
-          </Link>
-        </li>
-        <li className="main-nav__item main-nav__item--signup">
-          <Link to="/inscription" className="main-nav__link">
-            <Button label="S'inscrire" />
-          </Link>
-        </li>
+        {!isLogged && (
+          <>
+            <li className="main-nav__item">
+              <Link to="/connection" className="main-nav__link">
+                <Button label="Se connecter" type="outlined" />
+              </Link>
+            </li>
+            <li className="main-nav__item main-nav__item--signup">
+              <Link to="/inscription" className="main-nav__link">
+                <Button label="S'inscrire" />
+              </Link>
+            </li>
+          </>
+
+        )}
+        {isLogged && (
+          <li className="main-nav__item">
+            <Link to="/mon-compte" className="main-nav__link">
+              <Button label="Mon compte" icon="profil" />
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,19 +2,21 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-function Button({ label, type }) {
+function Button({ label, type, icon }) {
   return (
-    <button type="button" className={`button button--${type}`} title={label}>{label}</button>
+    <button type="button" className={`button button--${type}`} title={label}>{icon ? <img src={`images/icones/${icon}.svg`} alt="null" className="button__icon" /> : ''}{label}</button>
   );
 }
 
 Button.defaultProps = {
   type: 'plain',
+  icon: null,
 };
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 export default Button;
