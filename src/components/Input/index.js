a07e334ab367819c26f0449a47ee789a942c06ab
Raphael Desitter <rdesitter@gmail.com>
2022-09-23 17:52:53 +0200

    :rocket: update feat : Input component with jsdoc

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,6 +2,25 @@ import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeValue } from '../../actions';
 
+/**
+ * Custom Input component
+ * @component
+ * @example
+ * const name = email
+ * const props = {
+ *  className: "input__class",
+ *  placeholder: "your placeholder"
+ * }
+ * return (
+ *  <Input
+ *    name={name}
+ *    className={className}
+ *    placeholder={placeholder}
+ *    value=""
+ *    onChange={handleChange}
+ *  />
+ * )
+ */
 function Input({ name, ...props }) {
   const value = useSelector((state) => state.user[name]);
   const dispatch = useDispatch();
