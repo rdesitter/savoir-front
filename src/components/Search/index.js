105160ca8b9ad4bf21b09bef4f21f594b6795f47
Raphael Desitter <rdesitter@gmail.com>
2022-09-26 19:09:06 +0200

    :hammer: fix Search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,66 +1,61 @@
 import PropTypes from 'prop-types';
-
-import Page from '../Page';
 import Panel from '../Panel';
-
 import './style.scss';
 
-function Search({ listOfCategories }) {
+function Search({ categories }) {
   return (
-    <Page>
-      <Panel className="search">
-        <form>
-          <fieldset>
+    <Panel className="search">
+      <form>
+        <fieldset>
 
-            <legend className="search-title">Recherche rapide</legend>
-            <div className="search-box">
+          <legend className="search-title">Recherche rapide</legend>
+          <div className="search-box">
 
-              {/* Search 1st step */}
-              <div className="search-choice">
-                <div className="search-choice__box">
-                  <div className="search-circle"><span className="search-circle__order">1</span></div>
-                  <label htmlFor="select-learn-share" className="search-text">Souhaitez vous apprendre ou partager&nbsp;?</label>
-                </div>
-                <select name="selection" id="select-learn-share" className="search-choice__select">
-                  <option value="">Choisissez une option...</option>
-                  <option value="Apprendre">Apprendre</option>
-                  <option value="Partager">Partager</option>
-                </select>
+            {/* Search 1st step */}
+            <div className="search-choice">
+              <div className="search-choice__box">
+                <div className="search-circle"><span className="search-circle__order">1</span></div>
+                <label htmlFor="select-learn-share" className="search-text">Souhaitez vous apprendre ou partager&nbsp;?</label>
               </div>
-
-              {/* Search step 2 */}
-              <div className="search-choice">
-                <div className="search-choice__box">
-                  <div className="search-circle"><span className="search-circle__order">2</span></div>
-                  <label htmlFor="select-category" className="search-text">Quelle catégorie vous intéresse&nbsp;?</label>
-                </div>
-                <select name="selection" id="select-category" className="search-choice__select">
-                  <option value="">Choisissez une catégorie...</option>
-                  {listOfCategories.map((category) => (
-                    <option value="{category.name}">{category.name}</option>
-                  ))}
-                </select>
-              </div>
-
-              {/* Search step 3 */}
-              <div className="search-launch">
-                <div className="search-launch__box">
-                  <div className="search-circle"><span className="search-circle__order">3</span></div>
-                  <span className="search-text">Lancez votre recherche</span>
-                </div>
-                <button type="submit" className="search-launch__button">Rechercher</button>
-              </div>
+              <select name="selection" id="select-learn-share" className="search-choice__select">
+                <option value="">Choisissez une option...</option>
+                <option value="Apprendre">Apprendre</option>
+                <option value="Partager">Partager</option>
+              </select>
             </div>
 
-          </fieldset>
-        </form>
-      </Panel>
-    </Page>
+            {/* Search step 2 */}
+            <div className="search-choice">
+              <div className="search-choice__box">
+                <div className="search-circle"><span className="search-circle__order">2</span></div>
+                <label htmlFor="select-category" className="search-text">Quelle catégorie vous intéresse&nbsp;?</label>
+              </div>
+              <select name="selection" id="select-category" className="search-choice__select">
+                <option value="">Choisissez une catégorie...</option>
+                {categories.map((category) => (
+                  <option key={category} value={category}>{category}</option>
+                ))}
+              </select>
+            </div>
+
+            {/* Search step 3 */}
+            <div className="search-launch">
+              <div className="search-launch__box">
+                <div className="search-circle"><span className="search-circle__order">3</span></div>
+                <span className="search-text">Lancez votre recherche</span>
+              </div>
+              <button type="submit" className="search-launch__button">Rechercher</button>
+            </div>
+          </div>
+
+        </fieldset>
+      </form>
+    </Panel>
   );
 }
 
 Search.propTypes = {
-  listOfCategories: PropTypes.arrayOf(
+  categories: PropTypes.arrayOf(
     PropTypes.string,
   ).isRequired,
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import postsReducer from './posts';
 import userReducer from './user';
+import categoriesReducer from './categories';
 
 const rootReducer = combineReducers({
   posts: postsReducer,
   user: userReducer,
+  categories: categoriesReducer,
 });
 
 export default rootReducer;
