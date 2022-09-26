907a24628d3f53317e26a8592e24a97a4bb1ec28
Raphael Desitter <rdesitter@gmail.com>
2022-09-26 18:41:54 +0200

    :rocket: feat : user AccountDetails component

---
+++ src/components/Accountdetails/index.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import Button from '../Button';
+
+import './style.scss';
+
+function AccountDetails({
+  id, username, avatar, created_at, about,
+}) {
+  const userId = useSelector((state) => state.user.userId);
+  const logged = useSelector((state) => state.user.logged);
+  return (
+    <div className="account">
+      <div className="account__avatar">
+        <img className="account__avatar__img" src={`/images/avatars/${avatar}.png`} alt={avatar} />
+      </div>
+      <div className="account__info">
+        <h2 className="account__title">{username}</h2>
+        <p className="account__creation">Inscrit depuis le {created_at}</p>
+        <h3 className="account__title">Description</h3>
+        <p className="account__about">{about}</p>
+        <span className="spacer" />
+        {logged && userId === id && <Button label="Modifier mon profil public" />}
+      </div>
+    </div>
+  );
+}
+
+AccountDetails.defaultProps = {
+  about: '',
+};
+
+AccountDetails.propTypes = {
+  username: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  created_at: PropTypes.string.isRequired,
+  about: PropTypes.string,
+  id: PropTypes.number.isRequired,
+};
+
+export default AccountDetails;
