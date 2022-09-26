27827eaff4fc42b2f78f8a956e5b77c9dfc62e2c
Raphael Desitter <rdesitter@gmail.com>
2022-09-26 10:38:14 +0200

    :hammer: fix : delete console.log

--- src/middlewares/fakeajax.js
+++ src/middlewares/fakeajax.js
@@ -5,9 +5,7 @@ const ajax = (store) => (next) => (action) => {
   const { user: { email, password } } = store.getState();
 
   if (action.type === LOGIN) {
-    console.log(users, email, password);
     const selectedUser = users.find((user) => user.email === email);
-    console.log(selectedUser);
     if (password === selectedUser.password) {
       store.dispatch(setUser(selectedUser.username));
     }
