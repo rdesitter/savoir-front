a27b2e6ed714852334a113d74e51b4e33f5cc16b
severine-cuenot <nine.injections@gmail.com>
2022-09-26 12:19:08 +0200

    :art: style : starting scss for search panel. Stillneed to add inputs

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,20 +1,25 @@
 import Page from '../Page';
 import Panel from '../Panel';
 
+import './style.scss';
+
 function Search() {
   return (
     <Page>
-      <Panel>
-        <h2 className="searchPanel__title">Recherche rapide</h2>
-        <div>
-          <div className="searchPanel__Choice">
-            1 Souhaitez-vous apprendre ou partager ?
+      <Panel className="search">
+        <h2 className="search-title">Recherche rapide</h2>
+        <div className="search-box">
+          <div className="search-choice">
+            <span className="search-order">1</span>
+            <span className="search-text">Souhaitez-vous apprendre ou partager&nbsp;?</span>
           </div>
-          <div className="searchPanel__Choice">
-            2 Quelle catégorie vous intéresse le plus ?
+          <div className="search-choice">
+            <span className="search-order">2</span>
+            <span className="search-text">Quelle catégorie vous intéresse le plus&nbsp;?</span>
           </div>
-          <div className="searchPanel__launch">
-            3 Lancez votre recherche
+          <div className="search-launch">
+            <span className="search-order">3</span>
+            <span className="search-text">Lancez votre recherche</span>
           </div>
         </div>
       </Panel>
