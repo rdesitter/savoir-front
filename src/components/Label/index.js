7b0ee321fb378935e7d068277e37b426aa0fd9cc
CecileBonnin <cecile.bonn1@gmail.com>
2022-09-26 10:51:17 +0200

    :wrench: config: setting up label component

---
+++ src/components/Label/index.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+function Label() {
+  return (
+    <div>Cuisine</div>
+  );
+}
+
+Label.propTypes = {
+
+};
+
+export default Label;
