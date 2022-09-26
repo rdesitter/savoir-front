fb3839a15ec3e79d4c285de6e37df439cf2b5002
severine-cuenot <nine.injections@gmail.com>
2022-09-26 14:19:37 +0200

    :art: style : arranging style. Still need to add inputs

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -10,16 +10,19 @@ function Search() {
         <h2 className="search-title">Recherche rapide</h2>
         <div className="search-box">
           <div className="search-choice">
-            <span className="search-order">1</span>
+            <div className="search-circle"><span className="search-circle__order">1</span></div>
             <span className="search-text">Souhaitez-vous apprendre ou partager&nbsp;?</span>
           </div>
           <div className="search-choice">
-            <span className="search-order">2</span>
+            <div className="search-circle"><span className="search-circle__order">2</span></div>
             <span className="search-text">Quelle catégorie vous intéresse le plus&nbsp;?</span>
           </div>
           <div className="search-launch">
-            <span className="search-order">3</span>
-            <span className="search-text">Lancez votre recherche</span>
+            <div className="search-launch__box">
+              <div className="search-circle"><span className="search-circle__order">3</span></div>
+              <span className="search-text">Lancez votre recherche</span>
+            </div>
+            <button type="submit" className="search-launch__button">Rechercher</button>
           </div>
         </div>
       </Panel>
