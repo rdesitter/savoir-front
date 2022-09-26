5f132d336554cd8f121e7c641af7c2d0e3b68168
severine-cuenot <nine.injections@gmail.com>
2022-09-26 14:52:37 +0200

    :wrench: config : added select inputs + some scss

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -9,14 +9,34 @@ function Search() {
       <Panel className="search">
         <h2 className="search-title">Recherche rapide</h2>
         <div className="search-box">
+
+          {/* Search 1st step */}
           <div className="search-choice">
-            <div className="search-circle"><span className="search-circle__order">1</span></div>
-            <span className="search-text">Souhaitez-vous apprendre ou partager&nbsp;?</span>
+            <div className="search-choice__box">
+              <div className="search-circle"><span className="search-circle__order">1</span></div>
+              <label htmlFor="select-learn-share" className="search-text">Souhaitez vous apprendre ou partager&nbsp;?</label>
+            </div>
+            <select name="selection" id="select-learn-share" className="search-choice__select">
+              <option value="">Choisissez une option...</option>
+              <option value="Apprendre">Apprendre</option>
+              <option value="Partager">Partager</option>
+            </select>
           </div>
+
+          {/* Search step 2 */}
           <div className="search-choice">
-            <div className="search-circle"><span className="search-circle__order">2</span></div>
-            <span className="search-text">Quelle catégorie vous intéresse le plus&nbsp;?</span>
+            <div className="search-choice__box">
+              <div className="search-circle"><span className="search-circle__order">1</span></div>
+              <label htmlFor="select-category" className="search-text">Quelle catégorie vous intéresse&nbsp;?</label>
+            </div>
+            <select name="selection" id="select-category" className="search-choice__select">
+              <option value="">Choisissez une catégorie...</option>
+              <option value="Apprendre">Apprendre</option>
+              <option value="Partager">Partager</option>
+            </select>
           </div>
+
+          {/* Search step 3 */}
           <div className="search-launch">
             <div className="search-launch__box">
               <div className="search-circle"><span className="search-circle__order">3</span></div>
