4a69a507b81065c2d9b1eb2345b677dd97868bd5
Raphael Desitter <rdesitter@gmail.com>
2022-09-27 19:01:13 +0200

    :rocket: feat : Post creation page V0

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 function Button({
-  label, btnstyle, isSubmit, icon,
+  label, btnstyle, isSubmit, icon, ...props
 }) {
   return (
-    <button tabIndex="-1" type={isSubmit ? 'submit' : 'button'} className={`button button--${btnstyle}`} title={label}>{icon ? <img src={`images/icones/${icon}.svg`} alt="null" className="button__icon" /> : ''}<span>{label}</span></button>
+    <button
+      tabIndex="-1"
+      type={isSubmit ? 'submit' : 'button'}
+      className={`button button--${btnstyle}`}
+      title={label}
+      {...props}
+    >
+      {icon ? <img src={`images/icones/${icon}.svg`} alt="null" className="button__icon" /> : ''}
+      <span>{label}</span>
+    </button>
   );
 }
 
