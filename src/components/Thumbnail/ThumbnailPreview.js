d8edb27c36692774b2666028a7788accd8e4c4c5
CecileBonnin <cecile.bonn1@gmail.com>
2022-09-28 11:18:04 +0200

    :rocket: feat: add dynamization color on <Label />

--- src/components/Thumbnail/ThumbnailPreview.js
+++ src/components/Thumbnail/ThumbnailPreview.js
@@ -24,8 +24,8 @@ function ThumbnailPreview({
           <h3 className="thumbnail-user__name">{name}</h3>
         </div>
       </Link>
-      <Label label="cuisine" />
-       <Link to={`/annonce/:${postId}`} title={`vers l'annonce "${title}"`}>
+      <Label label={category} color="light-grey" />
+      <Link to={`/annonce/:${postId}`} title={`vers l'annonce "${title}"`}>
         <div className="thumbnail-infos">
           <img className="thumbnail-infos__img" src={`./images/categories/${category}.jpg`} alt="Cuisine" />
           <h4 className="thumbnail-infos__title">{title}</h4>
