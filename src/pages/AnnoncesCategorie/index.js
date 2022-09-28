02355fcfb7fec6ac2c9b324fb805511ef121ed60
CecileBonnin <cecile.bonn1@gmail.com>
2022-09-28 14:59:29 +0200

    :wrench: config: add AnnonceCategorie folder

---
+++ src/pages/AnnoncesCategorie/index.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+function AnnoncesCategorie() {
+  return (
+    <div>AnnoncesCategorie</div>
+  );
+}
+
+AnnoncesCategorie.propTypes = {
+
+};
+
+export default AnnoncesCategorie;
