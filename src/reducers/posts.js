405bd8f0cdbb7fa15b5ffd19e0c0a2d2b897a5c3
CecileBonnin <cecile.bonn1@gmail.com>
2022-09-30 15:52:45 +0200

    :wrench: config: clean initial post state

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,19 +2,13 @@ export const initialState = {
   posts: [],
   loading: true,
   selectedPost: {
-    id: 1,
-    title: 'Mon annonce',
-    createdAt: '12/15/2022',
-    category: 'cuisine',
-    location: 'Montpellier',
-    description: 'Je souhaite apprendre à me servir (...)',
-    user: {
-      username: 'Jeanne',
-      email: 'jeanette@mail.fr',
-      createdAt: '23/05/2022',
-      avatar: 'NOHAIR1',
-      userId: 576,
-    },
+    id: '',
+    title: '',
+    createdAt: '',
+    category: '',
+    location: '',
+    description: '',
+    user: {},
   },
 };
 
