6e770db8040f70bc44924bd14f5e0e54b459643b
CecileBonnin <cecile.bonn1@gmail.com>
2022-09-30 15:55:00 +0200

    :wrench: config: add selectedPost middleware fie and add it in store

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,9 @@ import contactForm from '../middlewares/contactForm';
 import search from '../middlewares/search';
 import ajax from '../middlewares/ajax';
 import logOutSession from '../middlewares/logOutSession';
+import selectedPost from '../middlewares/selectedPost';
 
-const middlewares = applyMiddleware(contactForm, ajax, search, logOutSession);
+const middlewares = applyMiddleware(contactForm, ajax, search, logOutSession, selectedPost);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancers = composeEnhancers(middlewares);
