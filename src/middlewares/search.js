3ab617845d79be945aabc73ea897b8311b2b2bba
severine-cuenot <nine.injections@gmail.com>
2022-10-05 11:22:21 +0200

    :hammer: fix : CSS oh Search component is fixed!

--- src/middlewares/search.js
+++ src/middlewares/search.js
@@ -2,10 +2,8 @@ import axios from 'axios';
 import {
   LAUNCH_SEARCH,
   GET_CATEGORIES,
-  GET_POSTS_RESULTS,
   setCategories,
   setResults,
-  setPostsResults,
   toggleLoading,
 } from '../actions';
 
@@ -32,18 +30,6 @@ const search = (store) => (next) => (action) => {
     instance.get(`/api/annonces/category/${action.category}`)
       .then((response) => {
         store.dispatch(setResults(response.data));
-      })
-      .catch((error) => {
-      // en cas d’échec de la requête
-        console.log(error);
-        alert('Erreur de chargement, veuillez réessayer');
-      });
-  }
-
-  if (action.type === GET_POSTS_RESULTS) {
-    instance.get(`/api/annonces/category/${action.category}/type/${action.learnOrShare}`)
-      .then((response) => {
-        store.dispatch(setPostsResults(response.data));
       })
       .catch((error) => {
       // en cas d’échec de la requête
--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,5 +1,5 @@
 import {
-  CHANGE_CATEGORY_VALUE, CHANGE_LEARNORSHARE_VALUE, LAUNCH_SEARCH, SET_POSTS_RESULTS, SET_RESULTS,
+  CHANGE_CATEGORY_VALUE, CHANGE_LEARNORSHARE_VALUE, LAUNCH_SEARCH, SET_RESULTS,
 } from '../actions';
 
 export const initialState = {
@@ -35,12 +35,6 @@ const reducer = (state = initialState, action = {}) => {
       return {
         ...state,
         learnOrShareValue: action.value,
-      };
-    case SET_POSTS_RESULTS:
-      return {
-        ...state,
-        results: action.results,
-        resultsLoading: false,
       };
     default:
       return state;
