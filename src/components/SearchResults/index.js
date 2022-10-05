f973ddc4ab91193597ab051bee5fe6758624bf84
severine-cuenot <nine.injections@gmail.com>
2022-10-05 10:49:07 +0200

    :hammer: fix : search results ok !

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,52 +1,31 @@
-// import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import OneSearchResult from './OneSearchResult';
-import { getPostsResults } from '../../actions';
+import { useSelector } from 'react-redux';
+import ThumbnailPreview from '../Thumbnail/ThumbnailPreview';
+
 import './style.scss';
 
 function SearchResults() {
   const displayResults = useSelector((state) => state.search.results);
-  console.log('mes résultats', displayResults);
-  const loading = useSelector((state) => state.search.resultsLoading);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getPostsResults());
-  }, []);
 
   return (
     <>
       <div className="thumbnails-type">
         <h2 className="thumbnails-type__title">Résultats de la recherche</h2>
       </div>
-      {loading && <div>Loading...</div>}
-      {!loading
-          && (
-          <div className="thumbnails">
-            {displayResults.map((post) => (
-              <OneSearchResult
-                key={post.id}
-                avatar={post.user.avatar}
-                name={post.user.username}
-                userId={post.user.userId}
-                category={post.category}
-                postId={post.id}
-                title={post.title}
-              />
-            ))}
-          </div>
-          )}
+      <div className="thumbnails">
+        {displayResults.map((post) => (
+          <ThumbnailPreview
+            key={post.id}
+            avatar="Toto"
+            name="Tata"
+            category="Bidon"
+            title={post.title}
+            userId={post.user_id}
+            postId={post.id}
+          />
+        ))}
+      </div>
     </>
   );
 }
-
-// SearchResults.defaultProps = {
-//   results: [],
-// };
-
-// SearchResults.propTypes = {
-//   results: PropTypes.array,
-// };
 
 export default SearchResults;
