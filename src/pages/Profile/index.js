56439c51c998e56c5010727527ff8133f7739cb1
Benoit-Castang <103994311+Benoit-Castang@users.noreply.github.com>
2022-10-05 14:46:57 +0200

    :hammer: fix : format date

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import AccountDetails from '../../components/Accountdetails';
@@ -6,19 +6,15 @@ import Button from '../../components/Button';
 import Container from '../../components/Container';
 import Page from '../../components/Page';
 import Panel from '../../components/Panel';
-import UserPosts from '../../components/UserPosts';
-// import getUSerProfile from '../../selectors/getUserProfile';
+import PostPreview from '../../components/PostPreview';
 import { getUsers } from '../../actions';
 import './style.scss';
-import PostPreview from '../../components/PostPreview';
 
 function Profile() {
   const isAdmin = useSelector((state) => state.user.admin);
   const user = useSelector((state) => state.user.userProfil);
   const userPosts = useSelector((state) => state.posts.selectedUserPost);
   const { id } = useParams();
-  // const [userProfile, setUserProfile] = useState({});
-  // const [loading, setLoading] = useState(true);
   const loading = useSelector((state) => state.user.userLoading);
   const postLoading = useSelector((state) => state.posts.loadingSelectedPost);
   console.log('USERPOST', userPosts);
@@ -48,25 +44,25 @@ function Profile() {
           />
           )}
         </Panel>
+        
         <Panel>
-            <h2 className="post-previews__title">Les annonces</h2>
-              {postLoading && <div>Loading...</div>}
-              {!postLoading
-              && (
-                <div className="post-previews">
-                  {userPosts.map((userPost) => (
-                      <PostPreview
-                        cover={userPost.category_slug}
-                        category={userPost.category_name}
-                        title={userPost.title}
-                        created_at={userPost.created_at}
-                        id={userPost.id}
-                        key={userPost.id}
-                      />
-                  ))}
-                </div>
-              )}
-          
+          {postLoading && <div>Loading...</div>}
+            <h2 className="post-previews__title">Les annonces de {user.pseudo}</h2>
+            {!postLoading
+            && (
+            <div className="post-previews">
+              {userPosts.map((userPost) => (
+                <PostPreview
+                  cover={userPost.category_slug}
+                  category={userPost.category_name}
+                  title={userPost.title}
+                  created_at={userPost.created_at}
+                  id={userPost.id}
+                  key={userPost.id}
+                  />
+              ))}
+            </div>
+            )}
         </Panel>
       </Container>
     </Page>
