567356ef81c347aaea018e05d7932c13e90a20b9
CecileBonnin <cecile.bonn1@gmail.com>
2022-10-10 11:03:38 +0200

    :rocket: feat: add loggout button

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,14 +1,24 @@
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+// import variables from '../../styles/variables.modules.scss';
 import Button from '../Button';
 import Logo from '../Logo';
 import searchIcon from './search.svg';
+import { logOut } from '../../actions';
 
 import './style.scss';
 
 function AppHeader() {
   const isLogged = useSelector((state) => state.user.logged);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // const { redColor } = variables;
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+    navigate('/');
+  };
   return (
     <nav className="main-nav" aria-label="Navigation">
       <Link to="/" className="main-nav__logo"><Logo /></Link>
@@ -36,6 +46,7 @@ function AppHeader() {
             <Link to="/mon-compte" className="main-nav__link">
               <Button label="Mon compte" icon="profil" />
             </Link>
+            <Button label="Me déconnecter" style={{ backgroundColor: 'red' }} onClick={handleLogOut} />
           </li>
         )}
       </ul>
