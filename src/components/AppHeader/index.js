c16d25e0e8cc506615fcb51069bd7fe6651ea254
Raphael Desitter <rdesitter@gmail.com>
2022-10-10 15:34:17 +0200

    :art: update logout button

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,10 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-// import variables from '../../styles/variables.modules.scss';
 import Button from '../Button';
 import Logo from '../Logo';
 import searchIcon from './search.svg';
-import variables from '../../styles/variables.module.scss';
 import { logOut } from '../../actions';
 
 import './style.scss';
@@ -14,7 +12,6 @@ function AppHeader() {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { redColor } = variables;
 
   const handleLogOut = () => {
     dispatch(logOut());
@@ -47,9 +44,9 @@ function AppHeader() {
             <Link to="/mon-compte" className="main-nav__link">
               <Button label="Mon compte" icon="profil" />
             </Link>
-            <Link to="/" className="main-nav__link">
-              <Button label="Me déconnecter" style={{ backgroundColor: redColor }} onClick={handleLogOut} />
-            </Link>
+            <button className="button button--logout" type="button" onClick={handleLogOut} title="Se déconnecter">
+              <span className="material-symbols-outlined">logout</span>
+            </button>
           </li>
         )}
       </ul>
