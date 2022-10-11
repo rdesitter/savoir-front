395053a82b19952578176f36b0795a46f18454e6
severine-cuenot <nine.injections@gmail.com>
2022-10-11 13:50:58 +0200

    :wrench: config : cleaning middleware because can't make it works

--- src/middlewares/resetEmail.js
+++ src/middlewares/resetEmail.js
@@ -1,51 +1,19 @@
 import axios from 'axios';
 import {
-  RESET_EMAIL, setError, toggleSavedData,
+  resetEmail, setError, SUBMIT_NEW_EMAIL,
 } from '../actions';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-const getUserData = (user) => ({
-  admin: user.admin,
-  userId: user.userId,
-  avatarId: user.avatarId,
-  birthdate: user.birthdate,
-  created_at: user.created_at,
-  description: user.description,
-  email: user.email,
-  firstname: user.firstname,
-  lastname: user.lastname,
-  logged: user.logged,
-  postalCode: user.postalCode,
-  pronoun: user.pronoun,
-  roleId: user.roleId,
-  userProfil: user.userProfil,
-  username: user.username,
-});
-
-const tokenConfig = {
-  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-};
-
 const ResetEmail = (store) => (next) => (action) => {
-  if (action.type === RESET_EMAIL) {
+  if (action.type === SUBMIT_NEW_EMAIL) {
     try {
       const { user } = store.getState();
-      const { userId, email } = user;
-      const userData = getUserData(user);
-
-      instance.patch(`/api/user/${userId}`, { email }, tokenConfig)
+      instance.patch(`/api/user/${user.userId}`)
         .then(() => {
-          localStorage.setItem('user', JSON.stringify(userData));
-          store.dispatch(toggleSavedData());
-        })
-        .catch((error) => {
-          if (error.response.status === 401) {
-            return store.dispatch(setError('Vous n\'êtes pas autorisé à faire cette modification'));
-          }
-          return store.dispatch(setError(error.message));
+          
         });
     }
     catch (error) {
